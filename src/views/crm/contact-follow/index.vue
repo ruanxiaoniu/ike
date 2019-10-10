d4e793918691bb0feb2c14a663c869eb8bbf060d
lan <2317735669@qq.com>
2019-10-10 08:45:27 +0800

    跟进模块更新

--- src/views/crm/contact-follow/index.vue
+++ src/views/crm/contact-follow/index.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>
-    联系跟进
+  <div class="tab-container">
+    <el-tabs v-model="activeName" style="margin-top:15px;" type="border-card">
+      <el-tab-pane v-for="item in tabMapOptions" :key="item.key" :label="item.label" :name="item.key">
+        <keep-alive>
+          <tab-pane v-if="activeName==item.key" :type="item.key"  />
+        </keep-alive>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
+
+<script>
+import tabPane from '../allcustomers/TabPane/index'
+
+export default {
+  name: 'Tab',
+  components: { tabPane },
+  data() {
+    return {
+      tabMapOptions: [
+        { label: '全部', key: 'all' },
+        { label: '今日跟进', key: 'today_follow' },
+        { label: '明天跟进', key: 'tomorrow_follow' },
+        { label: '后天跟进', key: 'after_tomorrow' },
+        { label: '本周跟进', key: 'this_week' }
+      ],
+      activeName:'all',
+      modelType:'myCustomer'
+    }
+  },
+  watch: {
+    activeName(val) {
+      this.$router.push(`${this.$route.path}?tab=${val}`)
+    }
+  },
+  created() {
+    // init the default selected tab
+    const tab = this.$route.query.tab
+    if (tab) {
+      this.activeName = tab
+    }
+    this.$store.dispatch('customer/setmodelType','myCustomer')
+  },
+  mounted(){
+    
+  },
+  methods: {
+    showCreatedTimes() {
+      this.createdTimes = this.createdTimes + 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-container {
+    margin: 30px;
+  }
+</style>
--- src/views/crm/follow-plan/index.vue
+++ src/views/crm/follow-plan/index.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>
-    跟进计划
+  <div class="tab-container">
+    <el-tabs v-model="activeName" style="margin-top:15px;" type="border-card">
+      <el-tab-pane v-for="item in tabMapOptions" :key="item.key" :label="item.label" :name="item.key">
+        <keep-alive>
+          <tab-pane v-if="activeName==item.key" :type="item.key"  />
+        </keep-alive>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
+
+<script>
+import tabPane from '../allcustomers/TabPane/index'
+
+export default {
+  name: 'Tab',
+  components: { tabPane },
+  data() {
+    return {
+      tabMapOptions: [
+        { label: '全部', key: 'all' },
+        { label: '今日新增', key: 'today_add' },
+        { label: '今天跟进', key: 'today_follow' },
+        { label: '30天未跟进', key: 'thirty' },
+        { label: '从未跟进', key: 'never' }
+      ],
+      activeName:'all',
+      modelType:'myCustomer'
+    }
+  },
+  watch: {
+    activeName(val) {
+      this.$router.push(`${this.$route.path}?tab=${val}`)
+    }
+  },
+  created() {
+    // init the default selected tab
+    const tab = this.$route.query.tab
+    if (tab) {
+      this.activeName = tab
+    }
+    this.$store.dispatch('customer/setmodelType','myCustomer')
+  },
+  mounted(){
+    
+  },
+  methods: {
+    showCreatedTimes() {
+      this.createdTimes = this.createdTimes + 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-container {
+    margin: 30px;
+  }
+</style>
